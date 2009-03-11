7fb882db9e5547c19675c0dfc8713a430f64b52a
mahb <mahb@mahb-desktop.(none)>
2009-03-11 20:53:06 +0800

    加入特征,用户管理

--- features/step_definitions/webrat_steps.rb
+++ features/step_definitions/webrat_steps.rb
@@ -3,6 +3,10 @@ require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "pat
 # Commonly used webrat steps
 # http://github.com/brynary/webrat
 
+Given /^I am on (.+)$/ do |page_name|
+  visit path_to(page_name)
+end
+
 When /^I go to (.+)$/ do |page_name|
   visit path_to(page_name)
 end
@@ -42,7 +46,7 @@ When /^I select "(.*)" as the "(.*)" date and time$/ do |datetime, datetime_labe
   select_datetime(datetime, :from => datetime_label)
 end
 
-# Use this step in conjuction with Rail's time_select helper. For example:
+# Use this step in conjunction with Rail's time_select helper. For example:
 # When I select "2:20PM" as the time
 # Note: Rail's default time helper provides 24-hour time-- not 12 hour time. Webrat
 # will convert the 2:20PM to 14:20 and then select it. 
@@ -57,7 +61,7 @@ When /^I select "(.*)" as the "(.*)" time$/ do |time, time_label|
   select_time(time, :from => time_label)
 end
 
-# Use this step in conjuction with Rail's date_select helper.  For example:
+# Use this step in conjunction with Rail's date_select helper.  For example:
 # When I select "February 20, 1981" as the date
 When /^I select "(.*)" as the date$/ do |date|
   select_date(date)
@@ -82,16 +86,16 @@ When /^I choose "(.*)"$/ do |field|
   choose(field)
 end
 
-When /^I attach the file at "(.*)" to "(.*)" $/ do |path, field|
+When /^I attach the file at "(.*)" to "(.*)"$/ do |path, field|
   attach_file(field, path)
 end
 
 Then /^I should see "(.*)"$/ do |text|
-  response.body.should =~ /#{text}/m
+  response.should contain(text)
 end
 
 Then /^I should not see "(.*)"$/ do |text|
-  response.body.should_not =~ /#{text}/m
+  response.should_not contain(text)
 end
 
 Then /^the "(.*)" checkbox should be checked$/ do |label|
