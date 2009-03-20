d2cef1a686be2171533ff0ceeb7be46f7a4600b9
mahb <mahb@mahb-desktop.(none)>
2009-03-20 10:58:27 +0800

    增加新增来电页面中的输入项

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090312073914) do
+ActiveRecord::Schema.define(:version => 20090311141723) do
 
   create_table "users", :force => true do |t|
     t.string   "login"
