84a3d7cc6815310a8d84356030e46efd14d8ae8b
mahb <mahb@mahb-desktop.(none)>
2009-03-24 11:02:21 +0800

    修改日期控件中的中文显示及显示格式

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -32,5 +32,17 @@ j(document).ready(function(){
     }
   });
 
-  new SubMenu("t-menu")
+  new SubMenu("t-menu");
+
+  //calendar
+  _translations = {
+    "OK": "OK",
+    "Now": "Now",
+    "Today": "Today",
+    "Clear": "清空"
+  };
+  Date.weekdays = $w("日 一 二 三 四 五 六");
+  Date.months = $w("一月 二月 三月 四月 五月 六月 七月 八月 九月 十月 十一月 十二月");
+
+
 });
