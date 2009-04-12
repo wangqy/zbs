8d5d22c61a495df74c85bcfb9989557ed0a89e72
mahb <mahb@mahb-desktop.(none)>
2009-04-12 21:38:40 +0800

    根据事件类型在列表,增删查页面中显示不同的label

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,21 @@
 module EventsHelper
+  #@param "callnumber"
+  #@return 来电电话
+  def l(field)
+    t("activerecord.attributes.#{params[:type].downcase}.#{field}")
+  end
+  
+  #字段中文label,必填
+  #@param "callnumber"
+  #@return 来电电话
+  def lr(field)
+    "#{l(field)} #{require_star}"
+  end
+
+  #@param @event
+  #@param "callnumber"
+  #@return 来电电话
+  def el(field)
+    t("activerecord.attributes.#{@event.class.model_name.downcase}.#{field}")
+  end
 end
