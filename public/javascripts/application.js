7ac29193a396f23742af83f0eb99d94f74b21330
saberma <saberma@saberma-desktop.(none)>
2009-06-11 17:01:40 +0800

    修改呼叫中心样式,修改来电弹窗样式(去掉滚动栏)

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,46 +3,8 @@
 var j = jQuery.noConflict();
 
 j(document).ready(function(){
-  //make sure sbar's height the same as mainbar
-  if(j("#sbar").height()<j("#mainbar").height())
-    j("#sbar").height(j("#mainbar").height());
   //focus
   j(":input[is_focus]").focus();
-
-  //facebox lightbox(avoid enter key triggle another facebox)
-  j('a[rel*=facebox]').focus(function(){j(this).blur();});
-  j('a[rel*=facebox]').facebox(); 
-
-  j(document).bind('reveal.facebox', function(){
-    //focus
-    j(":input[is_focus]").focus();
-  });
-
-  var SubMenu = Class.create({
-    initialize: function(li) {
-      if(!$(li)) return;
-      this.trigger = $(li).down('em');
-      if(!this.trigger) return;
-      this.menu = $(li).down('ul');
-      this.trigger.observe('click', this.respondToClick.bind(this));
-      document.observe('click', function(){ this.menu.hide();}.bind(this));
-    },
-                  
-    respondToClick: function(event) {
-      event.stop();
-      $$('ul.submenu').without(this.menu).invoke('hide');
-      this.menu.toggle();
-    }
-  });
-
-  //根据主菜单的class为".mainmenu"来自动初始化子菜单
-  //new SubMenu("t-menu");
-  //new SubMenu("d-menu");
-  var menus = j("li.mainmenu");
-  menus.each(function(){
-    new SubMenu(j(this).attr("id"));
-  });
-
 
   //calendar
   _translations = {
