019a0430215ed37c7de067b3cf8a8efb5f852b71
saberma <saberma@saberma-desktop.(none)>
2009-06-18 14:15:06 +0800

    修改播放器样式

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -16,5 +16,15 @@ j(document).ready(function(){
   Date.weekdays = $w("日 一 二 三 四 五 六");
   Date.months = $w("一月 二月 三月 四月 五月 六月 七月 八月 九月 十月 十一月 十二月");
 
+  //wav player
+  j('.player').toggle(function(){
+    j(this).addClass('play_stop');
+    j('.player').removeClass('play_start');
+    j('#player').html("<bgsound src='"+j(this).attr("wav_url")+"' loop='infinite'>");
+  },function(){
+    j(this).addClass('play_start');
+    j('.player').removeClass('play_stop');
+    j('#player').html("");
+  });
 
 });
