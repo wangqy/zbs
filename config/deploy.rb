7c0f79c02c67253c82fdb68a80ce842a49c92598
saberma <saberma@saberma-desktop.(none)>
2009-06-19 10:37:24 +0800

    修正apt-packget包含nginx等多余配置导致deprec报错的问题

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,17 +20,23 @@ set :web_server_type, :nginx
 set :app_server_type, :mongrel
 set :db_server_type, :mysql
 
+set :mongrel_conf, "#{deploy_to}/current/config/mongrel_cluster.yml"
+
 role :app, domain
 role :web, domain
 role :db,  domain, :primary => true
+#has problem, so comment it
 set :database_yml_in_scm, false
 
-set :user, "ubuntu"
+set :user, "cogentzbs"
 set :runner, nil
 
-set :packages_for_project, %w(ruby rubygems libopenssl-ruby1.8 mysql-server mysql-client libmysqlclient15-dev git-core imagemagick nginx ruby1.8-dev libxslt1-dev libxml2-dev unixodbc unixodbc-dev)
-set :gems_for_project, %w(rails rspec hoe haml paperclip mislav-will_paginate mysql mongrel mongrel_cluster aslakhellesoy-cucumber webrat javan-whenever calendar_date_select)
-set :shared_dirs, %w(public/upload)
+set :packages_for_project, %w(libxslt1-dev libxml2-dev imagemagick)
+set :gems_for_project, %w(rspec hoe haml paperclip mislav-will_paginate mysql cucumber webrat)
+
+set :packages_for_project, %w(libopenssl-ruby1.8 libmysqlclient15-dev imagemagick libxslt1-dev libxml2-dev unixodbc unixodbc-dev)
+set :gems_for_project, %w(rspec rspec-rails hoe haml paperclip mislav-will_paginate aslakhellesoy-cucumber webrat javan-whenever calendar_date_select)
+set :shared_dirs, %w(public/upload log)
 
 namespace :deploy do
   task :restart, :roles => :app, :except => {:no_release => true } do
